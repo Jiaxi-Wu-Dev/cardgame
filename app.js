2a2e331ee5bdf628704b0773846455ba7f82b699
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-03-12 16:18:32 -0400

    adding card matching logic

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ let clickedTiles = null;
 
 function onTilesClicked(e) {
     const target = e.currentTarget
+
+    if(target.className.includes('done')){
+        return;
+    }
+
     target.className = target.className.replace("color", "color-hidden").trim()
     
     if(!clickedTiles){
         clickedCard = target; 
     } else if (clickedTiles){
-
+        if (clickedTiles.getAttribute('data-color') === target.getAttribute('data.color')) {
+            clickedTiles.className += " done"
+        }
     }
 }
