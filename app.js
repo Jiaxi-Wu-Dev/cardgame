a36e4596cc8d8fbc1189a433f6bca0a3b1517214
Jiaxi Wu <jiaxwu159@gmail.com>
2020-03-12 12:45:58 -0400

    reverted changes back to original grid

--- app.js
+++ app.js
@@ -6,14 +6,4 @@ class Squares {
     }
 }
 
-var canvas= document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
 
-for (i=1; i<=8; i++){
-    var x = math.floor(Math.random()*10)
-    var y = math.floor(Math.random()*10)
-
-    ctx.fillstyle = "blue";
-
-    ctx.fillRect(x, y, 15, 15);
-}
