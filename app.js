406788119bfb9ea279af76cfd837c5e57fe53533
Jiaxi Wu <jiaxwu159@gmail.com>
2020-03-12 11:41:54 -0400

    added some js

---
+++ app.js
@@ -0,0 +1,16 @@
+
+class Squares {
+    constructor(squareNum, squareColor) {
+        this.square = squareNum;
+        this.color = squareColor
+    }
+}
+
+function makeSquares() {
+    let totalSquares = 16
+    for (let i = 1; i < 16; i++){
+        //create a square with width 150px and height 150px
+        // assign it a random value starting from one, ending at 8
+        // assign it a color depending on the value 
+    }
+}
--- pseudocode.js
+++ pseudocode.js
@@ -4,8 +4,21 @@ need to use grid/flexbox to grid out the sections
 
 game should generate squares randomly each time the game is started 
 need a function that generates a grid with 8 matching colors if the colors match, they disappear
+using js they can be created by using an object class to make 16 sqaures using a for loop
+*/
+
+class MakeSquares {
+    constructor(squareNum, squareColor) {
+        this.square = squareNum;
+        this.color = squareColor
+    }
+}
+
+squareOne = new MakeSquares(1, "blue")
+console.log(squareOne)
 
 
-Use the WASD for player one and arrow keys for player two. For player one use the tab key to select the squares. For player two use the space key to select squares. In order to complete the game, the players need to empty the board of tiles by matching two of the same images. 
+
+/* Use the WASD for player one and arrow keys for player two. For player one use the tab key to select the squares. For player two use the space key to select squares. In order to complete the game, the players need to empty the board of tiles by matching two of the same images. 
 In a one player scenario theres a counter to see how fast you can clear the board
-In a two player scenario, theres two boards and whoever clears theres the fastest wins.  */
+In a two player scenario, theres two boards and whoever clears theres the fastest wins.   */
