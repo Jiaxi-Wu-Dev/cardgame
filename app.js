f30392336da565f38fa744a6b65452081f5f4339
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-03-12 15:45:15 -0400

    click tiles

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+let clickedTiles = null; 
 
 function onTilesClicked(e) {
     const target = e.currentTarget
-    console.log('clicked', e.currentTarget);
     target.className = target.className.replace("color", "color-hidden").trim()
+    
+    if(!clickedTiles){
+        clickedCard = target; 
+    } else if (clickedTiles){
+
+    }
 }
