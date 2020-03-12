a4a4681b472ed50710f80d37c1ce79de7477fdf4
Jiaxi Wu <jiaxwu159@gmail.com>
2020-03-12 10:29:51 -0400

    set up grid with divs in html

--- pseudocode.js
+++ pseudocode.js
@@ -1,5 +1,7 @@
 /* Pseudocode
-Create a layout of 3x3 with different backgrounds and layer it with different shapes
+-Create a layout of 3x3 with different backgrounds and layer it with different shapes
+need to use grid/flexbox to grid out the sections
+
 Use the WASD for player one and arrow keys for player two. For player one use the tab key to select the squares. For player two use the space key to select squares. In order to complete the game, the players need to empty the board of tiles by matching two of the same images. 
 In a one player scenario theres a counter to see how fast you can clear the board
 In a two player scenario, theres two boards and whoever clears theres the fastest wins.  */
