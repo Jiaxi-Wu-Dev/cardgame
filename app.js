a77082568604f4b33d000447767da82abac273bf
Jiaxi Wu <jiaxwu159@gmail.com>
2020-03-12 12:02:52 -0400

    created a function makeSqaures()

--- app.js
+++ app.js
@@ -6,11 +6,33 @@ class Squares {
     }
 }
 
+function draw() {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+      var context = canvas.getContext('2d');
+  
+      context.fillRect(20,20,100,100);
+      context.clearRect(40,40,60,60);
+      context.strokeRect(45,45,50,50);
+    }
+  }
+
+
 function makeSquares() {
-    let totalSquares = 16
-    for (let i = 1; i < 16; i++){
+    let totalSquares = 8
+    var canvas = document.getElementById('canvas');
+    for (let i = 1; i <= totalSquares; i++){
         //create a square with width 150px and height 150px
         // assign it a random value starting from one, ending at 8
         // assign it a color depending on the value 
+        if (canvas.getContext) {
+            var context = canvas.getContext('2d');
+
+            context.fillRect(20,20,100,100);
+            context.clearRect(40,40,60,60);
+            context.strokeRect(45,45,50,50);
+        }
     }
 }
+
+makeSquares();
