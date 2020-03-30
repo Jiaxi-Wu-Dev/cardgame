e0ec11de727ed6c015dee947738b137172ba77f2
Jiaxi Wu <dev.jiaxiwu@gmail.com>
2020-03-30 19:12:53 -0400

    comments

--- oneplayer.js
+++ oneplayer.js
@@ -10,8 +10,9 @@ function onCardClicked(e) {
     if (clickStop || marked === tilesClicked || marked.className.includes('done')) {
         return;
     }
-
+    // replace the hide-color attribute with nothing, showing the tiles colors
     marked.className = marked.className.replace('hide-color', '');
+    // appends the done attribute to the classname
     marked.className += ' done';
     //keep track of the card if its not clicked
     if (!tilesClicked) {
@@ -22,10 +23,15 @@ function onCardClicked(e) {
             // else if the tiles clicked, and the attribute color is not the same change change click to true
             tilesClicked.getAttribute('data-color') !== marked.getAttribute('data-color')) {
             click = true
+            //runs setTimeout function, flipping the tiles after 100ms
             setTimeout(() => {
+                //resets the className attributes back to hide color after done was appended
                 tilesClicked.className = tilesClicked.className.replace('done', '') + ' hide-color';
+                // resets the className attributes for markedTile back to hide color from done
                 marked.className = marked.className.replace('done', '') + ' hide-color';
+                // resets tilesClicked from marked to null
                 tilesClicked = null;
+                // resets click from true to false
                 click = false;
             }, 100);
 
@@ -33,7 +39,9 @@ function onCardClicked(e) {
             // win condition, have a alert when all the squares are done saying "congrats"
             numberCounter += 2;
             console.log(numberCounter)
-            tilesClicked = null;
+            tilesClicked = null
+            // since there are 16 tiles, when all 16 tiles are clicked the games over
+            // one way to keep track of that is to add 2 whenever two tiles are matched until the number is 16
             if (numberCounter >= 16) {
                 setTimeout(() => {
                     alert("Congradulations, you win!")
